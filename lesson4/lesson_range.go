ed65ace590b1453f2f8a9182c6263e045cfb7db1
田久保雄 <macbookpro15us2016@gmail.com>
2020-02-06 23:57:23 +0900

    :hammer: fix range

--- lesson4/lesson_range.go
+++ lesson4/lesson_range.go
@@ -17,4 +17,21 @@ func main() {
 	for _, v := range l {
 		fmt.Println(v)
 	}
+
+	//mapの値を取り出す時
+	m := map[string]int{"apple": 100, "banana": 200}
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	//keyだけ取り出したい時
+	for k := range m {
+		fmt.Println(k)
+	}
+
+	//valueだけ取り出したい時
+	for _, v := range m {
+		fmt.Println(v)
+	}
 }
